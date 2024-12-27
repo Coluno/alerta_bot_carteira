bacc533e1e9d73231a509070a7a2f4b8d4e62913
Gabriel Alencar <106896178+Coluno@users.noreply.github.com>
2024-12-27 11:41:26 -0300

    Update alert_dolar.py

--- alert_dolar.py
+++ alert_dolar.py
@@ -9,8 +9,8 @@ from sklearn.model_selection import train_test_split
 
 # Função para enviar mensagem via Telegram
 def send_telegram_message(message):
-    token = "q"
-    chat_id = "1"
+    token = "7654351807:AAHJ-Pve956eUVbRDMpF17iDG6Qv-V8zovM"
+    chat_id = "-1002473077704"
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     params = {"chat_id": chat_id, "text": message}
     response = requests.get(url, params=params)
